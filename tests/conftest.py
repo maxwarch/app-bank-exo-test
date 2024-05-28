6989e8c11fe328e204b33900311617d145958b7d
maxwarch <maxwarch@users.noreply.github.com>
2024-05-28 14:36:44 +0200

    comment conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from glob import glob
 def refactor(string: str) -> str:
     return string.replace("/", ".").replace("\\", ".").replace(".py", "")
 
-
+# permet d'importer tous les fichiers de fixture. Cela permet de "spécialiser" chaque fichier pour une meilleure lisibilité
 pytest_plugins = [
     refactor(fixture) for fixture in glob("tests/fixtures/*.py") if "__" not in fixture
 ]
